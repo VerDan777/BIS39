6a807e3b0509bdf770e2264182a3527f521667f0
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-09-02 23:51:39 +0200

    initialze work

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-// var source, destination;
-// var directionsDisplay;
+var source, destination;
+var directionsDisplay;
 
 
-// function initMap() {
-//     var directionsService = new google.maps.DirectionsService();
-// google.maps.event.addDomListener('window','load',function() {
-//     new google.maps.places.SearchBox(document.getElementById('evac-from-input'));
-//     new google.maps.places.SearchBox(document.getElementById('evac-to-input'));
-//     directionsDisplay = new google.maps.DirectionsRender({draggable: true});
-// }); 
-//     var mapDiv = document.querySelector(".map");
-//     var map = new google.maps.Map(mapDiv, {
-//         zoom: 10,
-//         center: {
-//             lat: 54.704,
-//             lng: 20.486
-//         }
-//     });
-//     directionsDisplay.setMap(map);
-//     directionsDisplay.setPanel(document.getElementById('dvPanel'));
-//     //Directions and display 
-//     source = document.getElementById('evac-from-input').value;
-//     destination = document.getElementById('evac-to-input').value;
-//     var request = {
-//         origin: source,
-//         destination: destination,
-//         travelMode: google.maps.TravelMode.DRIVING
-//     };
-//     directionsService.route(request,function(response,status){
-//         if(status==google.maps.DirectionsStatus.OK) {
-//             directionsDisplay.setDirections(response);
-//         }
-//     });
+function initMap() {
+    var directionsService = new google.maps.DirectionsService();
+google.maps.event.addDomListener('window','load',function() {
+    new google.maps.places.SearchBox(document.getElementById('evac-from-input'));
+    new google.maps.places.SearchBox(document.getElementById('evac-to-input'));
+    directionsDisplay = new google.maps.DirectionsRender({draggable: true});
+}); 
+    var mapDiv = document.querySelector(".map");
+    var map = new google.maps.Map(mapDiv, {
+        zoom: 10,
+        center: {
+            lat: 54.704,
+            lng: 20.486
+        }
+    });
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('dvPanel'));
+    //Directions and display 
+    source = document.getElementById('evac-from-input').value;
+    destination = document.getElementById('evac-to-input').value;
+    var request = {
+        origin: source,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request,function(response,status){
+        if(status==google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
+    });
 
-//     //*******DISTANCE AND DURATION********
-//     var service = new google.maps.DistanceMatrixService();
-//     service.getDistanceMatrix({
-//         origins: [source],
-//         destinations: [destination],
-//         travelMode: google.maps.TravelMode.DRIVING,
-//         unitSystem: google.maps.UnitSystem.METRIC,
-//         avoidHighways: false,
-//         avoidTolls: false
-//     },function(response,status){
-//         if(status== google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status !="ZERO_RESULTS") {
-//             var distance = response.rows[0].elements[0].distance.text;
-//             var duration = response.rows[0].elements[0].duration.text;
-//             var dvDistance = document.getElementById('dvDistance');
-//         }else {
-//             alert("Unable to find the distance via road");
-//         }
-//     })
+    //*******DISTANCE AND DURATION********
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix({
+        origins: [source],
+        destinations: [destination],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false
+    },function(response,status){
+        if(status== google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status !="ZERO_RESULTS") {
+            var distance = response.rows[0].elements[0].distance.text;
+            var duration = response.rows[0].elements[0].duration.text;
+            var dvDistance = document.getElementById('dvDistance');
+        }else {
+            alert("Unable to find the distance via road");
+        }
+    })
 
 
-// }
+}
    function initMap() {
         var directionsService = new google.maps.DirectionsService;
         var directionsDisplay = new google.maps.DirectionsRenderer;
